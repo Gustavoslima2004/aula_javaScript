e5d4a6ecbf890a77e3fdf719c9b56abb4fe7a601
Gustavoslima2004 <88349043+Gustavoslima2004@users.noreply.github.com>
2021-08-10 20:56:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("meu primeiro js");
+var nome = prompt("Qual seu nome?")
+var idade = prompt("Qual sua idade?")
+alert(nome + " tem " + idade + " anos, por tanto é:")
+if (idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
